b5f0b04bbbda30899f854d2d17c04961ccbc1c60
cpellerito1 <46943743+cpellerito1@users.noreply.github.com>
2022-02-02 11:09:53 -0500

    Updated how the input file was handled

--- src/compiler.java
+++ src/compiler.java
@@ -12,29 +12,32 @@ import java.util.ArrayList;
 public class compiler{
     public static void main(String[] args) throws Exception {
         // Use readFile method to read file from stand input into an ArrayList
-        ArrayList<String> inputFile = new ArrayList<String>();
+        ArrayList<char[]> inputFile = new ArrayList<char[]>();
         inputFile = readFile(args[0]);
+
+        for (int i = 0; i < inputFile.size(); i++)
+            System.out.println(inputFile.get(i));
 
     }
 
     /**
      * This class reads the input file from standard input and adds it
-     * to an ArrayList line by line
+     * to an ArrayList line by line as an array of characters
      * @param input
      * @return ArrayList containing the file line by line
      * @throws IOException
      */
-    public static ArrayList<String> readFile(String input) throws IOException {
+    public static ArrayList<char[]> readFile(String input) throws IOException {
         // Read file from standard input
         File inputFile = new File(input);
 
         // Initialize ArrayList to read the file to
-        ArrayList<String> inputString = new ArrayList<String>();
+        ArrayList<char[]> inputString = new ArrayList<char[]>();
 
         // Use try to catch errors while reading the input file line by line to the new ArrayList
         try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
             while (br.ready())
-                inputString.add(br.readLine());
+                inputString.add(br.readLine().toCharArray());
         }
 
         return inputString;
